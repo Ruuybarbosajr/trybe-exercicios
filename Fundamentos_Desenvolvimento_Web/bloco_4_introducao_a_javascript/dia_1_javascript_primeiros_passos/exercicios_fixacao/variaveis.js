6a3284e0aed1c5f6974f8821d9e2d6ce3bb379e6
Ruy <ruuybarbosajr@icloud.com>
2021-10-08 18:45:39 -0300

    adiciona exercicios de fixaçao

---
+++ Fundamentos_Desenvolvimento_Web/bloco_4_introducao_a_javascript/dia_1_javascript_primeiros_passos/exercicios_fixacao/variaveis.js
@@ -0,0 +1,11 @@
+const myName = 'Ruy';
+const birthCity = 'Teófilo Otoni'
+let birthYear = 1998
+birthYear = 2030
+
+birthCity = 'Governador Valadares'
+// Uma variavel const nao pode ter uma valor reatribuido.
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
