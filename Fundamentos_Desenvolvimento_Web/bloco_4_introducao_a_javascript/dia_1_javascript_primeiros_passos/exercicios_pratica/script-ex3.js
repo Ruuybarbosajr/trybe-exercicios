b52328e7d629ed3e005f59fa4c0551a545a52900
Ruy <ruuybarbosajr@icloud.com>
2021-10-08 21:35:52 -0300

    adiciona exercicios de pratica

---
+++ Fundamentos_Desenvolvimento_Web/bloco_4_introducao_a_javascript/dia_1_javascript_primeiros_passos/exercicios_pratica/script-ex3.js
@@ -0,0 +1,13 @@
+/* Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.*/
+
+const numberOne = 58;
+const numberTwo = 52;
+const numberThree = 47;
+
+if ( numberOne > numberTwo && numberOne > numberThree){
+  console.log(numberOne)
+}else if ( numberTwo > numberOne && numberTwo > numberThree ){
+  console.log(numberTwo)
+}else[
+  console.log(numberThree)
+]
