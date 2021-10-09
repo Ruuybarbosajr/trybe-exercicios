dea6d2a54a1e3d35b6b2284b70ffe9aaaaece4e2
Ruy <ruuybarbosajr@icloud.com>
2021-10-09 13:41:59 -0300

    adiciona exercicios completos

--- Fundamentos_Desenvolvimento_Web/bloco_4_introducao_a_javascript/dia_1_javascript_primeiros_passos/exercicios_pratica/script-ex8.js
+++ Fundamentos_Desenvolvimento_Web/bloco_4_introducao_a_javascript/dia_1_javascript_primeiros_passos/exercicios_pratica/script-ex8.js
@@ -4,7 +4,7 @@ const numberOne = 20;
 const numberTwo = 10;
 const numberThree = 15;
 
-if ( numberOne % 2 == 1 || numberTwo % 2 == 1 || numberThree % 2 == 1 ){
+if ( numberOne % 2 == 0 || numberTwo % 2 == 0 || numberThree % 2 == 0 ){
   console.log(true)
 }else{
   console.log(false)
