b07908e773f821a9a6ba0a749a601e968454ca3e
Ruy <ruuybarbosajr@icloud.com>
2021-10-11 20:04:25 -0300

    modificaçao exercicio

--- Fundamentos_Desenvolvimento_Web/bloco_4_introducao_a_javascript/dia_1_javascript_primeiros_passos/exercicios_pratica/script-ex6.js
+++ Fundamentos_Desenvolvimento_Web/bloco_4_introducao_a_javascript/dia_1_javascript_primeiros_passos/exercicios_pratica/script-ex6.js
@@ -1,9 +1,9 @@
 /* Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
  */
 
-let piece = 'RAINHA'.toLowerCase()
+let piece = 'peao'
 
-switch (piece){
+switch (piece.toLowerCase()){
   
   case 'rainha':
     console.log('A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.')
@@ -13,12 +13,17 @@ switch (piece){
     break;
   case 'bispo':
     console.log('O bispo move-se ao longo da diagonal. Não pode pular outras peças.')
+    break;
   case 'cavalo':
     console.log('É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.')
+    break;
   case 'torre':
     console.log(' torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.')
-  case 'peão':
+    break;
+  case 'peao':
     console.log('O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo')
+    break;
   default:
     console.log('erro')
+    break;
 }
